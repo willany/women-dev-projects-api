73a17c7c8d5cf00747d2e20a3f8579f8d9f67748
willanysilva <willany_lp@hotmail.com>
2020-03-29 23:44:02 -0300

    Add dotenv

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,11 @@
 const knex = require("knex");
 const configuration = require("../../knexfile");
+require("dotenv/config");
 
-const connection = knex(configuration.development);
+const env = process.env.NODE_ENV;
+
+const connection = knex(
+  env === "production" ? configuration.production : configuration.development
+);
 
 module.exports = connection;
